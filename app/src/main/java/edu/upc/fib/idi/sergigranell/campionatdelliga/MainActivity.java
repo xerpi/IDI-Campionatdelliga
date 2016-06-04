bfac8b4a56a7faf36f1f61a6de9c665dd258cdf3
Sergi Granell <xerpi.g.12@gmail.com>
2016-06-04 12:04:27 +0200

    Add initial Help and About Add app icon

--- app/src/main/java/edu/upc/fib/idi/sergigranell/campionatdelliga/MainActivity.java
+++ app/src/main/java/edu/upc/fib/idi/sergigranell/campionatdelliga/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.upc.fib.idi.sergigranell.campionatdelliga;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -88,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
 		return true;
 	}
@@ -96,13 +97,23 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
+		if (id == R.id.main_activity_action_help) {
+			return true;
+		} else if (id == R.id.main_activity_action_about) {
+			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+			// Setting Dialog Title
+			alertDialog.setTitle("Campionat de lliga");
+
+			// Setting Dialog Message
+			alertDialog.setMessage(
+				"Aplicació creada per Sergi Granell (sergi.granell@est.fib.upc.edu)\n" +
+				"Codi font: https://github.com/xerpi/IDI-Campionatdelliga");
+
+			// Showing Alert Message
+			alertDialog.show();
 			return true;
 		}
 
